e260cf255e695e6b71ed3c1231a3f91bb1cd206a
Gregory Danilenko <grigoriy.danilenko98@gmail.com>
2023-02-23 10:41:20 -0800

    dev

---
+++ M3/nya.tsx
@@ -0,0 +1,46 @@
+const concat = (str1: string, str2: string): string =>{
+  return str1 + ' ' + str2
+}
+
+concat('Hello', 'World')
+console.log(concat('Hello', 'World'))
+
+interface INya {
+  howIDoIt: string,
+  simeArray: (string | number)[],
+  withData: { howIDoIt: string, simeArray: (string | number)[] }[],
+}
+
+const MyHometask: INya = {
+  howIDoIt: "I Do It Wel",
+  simeArray: ["string one", "string two", 42],
+  withData: [{ howIDoIt: "I Do It Wel", simeArray: ["string one", 23] }],
+}
+
+interface MyArray<T> {
+  [key: number]: T;
+  reduce<T>(fn: (acc: T, el: T) => T, initial? : T): T;
+}
+
+const myArray: MyArray<number> = [1,2,3]
+console.log(myArray.reduce((accumulator, value) => accumulator + value, 0))
+
+interface IHomeTask {
+  data: string
+  numbericData: number
+  date: Date
+  externalData: {
+      basis: number
+      value: string
+  }
+}
+
+type MyPartial<T> = {
+  [N in keyof T]?: T[N] extends object ? MyPartial<T[N]> : T[N]
+}
+
+const homeTask: MyPartial<IHomeTask> = {
+  externalData: {
+    value: 'win'
+  }
+}
